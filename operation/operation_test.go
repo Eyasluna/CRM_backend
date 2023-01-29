32ce4599fc59700f5111062f3876096bc760b78d
Yibo Fu <yf091c@att.com>
2023-01-29 03:28:07 -0800

    fully functional version with init UT

---
+++ operation/operation_test.go
@@ -0,0 +1,29 @@
+package operation
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Suite")
+}
+
+var _ = Describe("View Node Test", func() {
+
+	BeforeEach(func() {
+
+	})
+	AfterEach(func() {
+
+	})
+
+	Context("when calculating node hash", func() {
+		It("should provide different hash if custom attributes are different", func() {
+
+		})
+
+	})
+})
